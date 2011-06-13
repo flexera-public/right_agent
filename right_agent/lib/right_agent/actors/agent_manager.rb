afcccaf4a3348f0a538826389a40bb43259162eb
Lee Kirchhoff <lee@rightscale.com>
2011-06-12 18:25:09 -0700

    Put actors inside RightScale module

--- right_agent/lib/right_agent/actors/agent_manager.rb
+++ right_agent/lib/right_agent/actors/agent_manager.rb
@@ -22,150 +22,156 @@
 
 require 'socket'
 
-class AgentManager
+module RightScale
 
-  include RightScale::Actor
+  # Generic manager for all agents to provide basic services
+  class AgentManager
 
-  expose :ping, :stats, :set_log_level, :execute, :connect, :disconnect
+    include Actor
+    include OperationResultHelper
 
-  # Valid log levels
-  LEVELS = [:debug, :info, :warn, :error, :fatal]
+    expose :ping, :stats, :set_log_level, :execute, :connect, :disconnect
 
-  # Initialize broker
-  #
-  # === Parameters
-  # agent(RightScale::Agent):: This agent
-  def initialize(agent)
-    @agent = agent
-  end
+    # Valid log levels
+    LEVELS = [:debug, :info, :warn, :error, :fatal]
 
-  # Always return success along with identity, protocol version, and broker information
-  # Used for troubleshooting
-  #
-  # === Return
-  # res(RightScale::OperationResult):: Always returns success
-  def ping(_)
-    res = RightScale::OperationResult.success(:identity => @agent.options[:identity],
-                                              :hostname => Socket.gethostname,
-                                              :version  => RightScale::AgentConfig.protocol_version,
-                                              :brokers  => @agent.broker.status,
-                                              :time     => Time.now.to_i)
-  end
-
-  # Retrieve statistics about agent operation
-  #
-  # === Parameters:
-  # options(Hash):: Request options:
-  #   :reset(Boolean):: Whether to reset the statistics after getting the current ones
-  #
-  # === Return
-  # res(RightScale::OperationResult):: Always returns success
-  def stats(options)
-    res = @agent.stats(RightScale::SerializationHelper.symbolize_keys(options))
-  end
-
-  # Change log level of agent
-  #
-  # === Parameter
-  # level(Symbol|String):: One of :debug, :info, :warn, :error, :fatal
-  #
-  # === Return
-  # res(RightScale::OperationResult):: Success if level was changed, error otherwise
-  def set_log_level(level)
-    level = level.to_sym if level.is_a?(String)
-    res = if LEVELS.include?(level)
-      RightScale::Log.level = level
-      RightScale::OperationResult.success
-    else
-      RightScale::OperationResult.error("Invalid log level '#{level.to_s}'")
+    # Initialize broker
+    #
+    # === Parameters
+    # agent(Agent):: This agent
+    def initialize(agent)
+      @agent = agent
     end
-  end
 
-  # Eval given code in context of agent
-  #
-  # === Parameter
-  # code(String):: Code to be eval'd
-  #
-  # === Return
-  # res(RightScale::OperationResult):: Success with result if code didn't raise an exception
-  #                                    Failure with exception message otherwise
-  def execute(code)
-    begin
-      return RightScale::OperationResult.success(self.instance_eval(code))
-    rescue Exception => e
-      return RightScale::OperationResult.error(e.message + " at\n  " + e.backtrace.join("\n  "))
+    # Always return success along with identity, protocol version, and broker information
+    # Used for troubleshooting
+    #
+    # === Return
+    # (OperationResult):: Always returns success
+    def ping(_)
+      success_result(:identity => @agent.options[:identity],
+                     :hostname => Socket.gethostname,
+                     :version  => AgentConfig.protocol_version,
+                     :brokers  => @agent.broker.status,
+                     :time     => Time.now.to_i)
     end
-  end
 
-  # Connect agent to an additional broker or reconnect it if connection has failed
-  # Assumes agent already has credentials on this broker and identity queue exists
-  #
-  # === Parameters
-  # options(Hash):: Connect options:
-  #   :host(String):: Host name of broker
-  #   :port(Integer):: Port number of broker
-  #   :id(Integer):: Small unique id associated with this broker for use in forming alias
-  #   :priority(Integer|nil):: Priority position of this broker in list for use
-  #     by this agent with nil meaning add to end of list
-  #   :force(Boolean):: Reconnect even if already connected
-  #
-  # === Return
-  # res(RightScale::OperationResult):: Success unless exception is raised
-  def connect(options)
-    options = RightScale::SerializationHelper.symbolize_keys(options)
-    res = RightScale::OperationResult.success
-    begin
-      if error = @agent.connect(options[:host], options[:port], options[:id], options[:priority], options[:force])
-        res = RightScale::OperationResult.error(error)
+    # Retrieve statistics about agent operation
+    #
+    # === Parameters:
+    # options(Hash):: Request options:
+    #   :reset(Boolean):: Whether to reset the statistics after getting the current ones
+    #
+    # === Return
+    # (OperationResult):: Always returns success
+    def stats(options)
+      @agent.stats(SerializationHelper.symbolize_keys(options))
+    end
+
+    # Change log level of agent
+    #
+    # === Parameter
+    # level(Symbol|String):: One of :debug, :info, :warn, :error, :fatal
+    #
+    # === Return
+    # res(OperationResult):: Success if level was changed, error otherwise
+    def set_log_level(level)
+      level = level.to_sym if level.is_a?(String)
+      res = if LEVELS.include?(level)
+        Log.level = level
+        success_result
+      else
+        error_result("Invalid log level '#{level.to_s}'")
       end
-    rescue Exception => e
-      res = RightScale::OperationResult.error("Failed to connect to broker: #{e.message}")
     end
-    res
-  end
 
-  # Disconnect agent from a broker
-  #
-  # === Parameters
-  # options(Hash):: Connect options:
-  #   :host(String):: Host name of broker
-  #   :port(Integer):: Port number of broker
-  #   :remove(Boolean):: Remove broker from configuration in addition to disconnecting it
-  #
-  # === Return
-  # res(RightScale::OperationResult):: Success unless exception is raised
-  def disconnect(options)
-    options = RightScale::SerializationHelper.symbolize_keys(options)
-    res = RightScale::OperationResult.success
-    begin
-      if error = @agent.disconnect(options[:host], options[:port], options[:remove])
-        res = RightScale::OperationResult.error(error)
+    # Eval given code in context of agent
+    #
+    # === Parameter
+    # code(String):: Code to be eval'd
+    #
+    # === Return
+    # (OperationResult):: Success with result if code didn't raise an exception,
+    #    otherwise failure with exception message
+    def execute(code)
+      begin
+        success_result(self.instance_eval(code))
+      rescue Exception => e
+        error_result(Log.format("Execute failed", e, :trace))
       end
-    rescue Exception => e
-      res = RightScale::OperationResult.error("Failed to disconnect from broker: #{e.message}")
     end
-    res
-  end
-
-  # Declare one or more broker connections unusable because connection setup has failed
-  #
-  # === Parameters
-  # options(Hash):: Failure options:
-  #   :brokers(Array):: Identity of brokers
-  #
-  # === Return
-  # res(RightScale::OperationResult):: Success unless exception is raised
-  def connect_failed(options)
-    options = RightScale::SerializationHelper.symbolize_keys(options)
-    res = RightScale::OperationResult.success
-    begin
-      if error = @agent.connect_failed(options[:brokers])
-        res = RightScale::OperationResult.error(error)
+  
+    # Connect agent to an additional broker or reconnect it if connection has failed
+    # Assumes agent already has credentials on this broker and identity queue exists
+    #
+    # === Parameters
+    # options(Hash):: Connect options:
+    #   :host(String):: Host name of broker
+    #   :port(Integer):: Port number of broker
+    #   :id(Integer):: Small unique id associated with this broker for use in forming alias
+    #   :priority(Integer|nil):: Priority position of this broker in list for use
+    #     by this agent with nil meaning add to end of list
+    #   :force(Boolean):: Reconnect even if already connected
+    #
+    # === Return
+    # res(OperationResult):: Success unless exception is raised
+    def connect(options)
+      options = SerializationHelper.symbolize_keys(options)
+      res = success_result
+      begin
+        if error = @agent.connect(options[:host], options[:port], options[:id], options[:priority], options[:force])
+          res = error_result(error)
+        end
+      rescue Exception => e
+        res = error_result("Failed to connect to broker: #{e.message}")
       end
-    rescue Exception => e
-      res = RightScale::OperationResult.error("Failed to notify agent that brokers #{options[:brokers]} are unusable: #{e.message}")
+      res
     end
-    res
-  end
 
-end
+    # Disconnect agent from a broker
+    #
+    # === Parameters
+    # options(Hash):: Connect options:
+    #   :host(String):: Host name of broker
+    #   :port(Integer):: Port number of broker
+    #   :remove(Boolean):: Remove broker from configuration in addition to disconnecting it
+    #
+    # === Return
+    # res(OperationResult):: Success unless exception is raised
+    def disconnect(options)
+      options = SerializationHelper.symbolize_keys(options)
+      res = success_result
+      begin
+        if error = @agent.disconnect(options[:host], options[:port], options[:remove])
+          res = error_result(error)
+        end
+      rescue Exception => e
+        res = error_result("Failed to disconnect from broker: #{e.message}")
+      end
+      res
+    end
+
+    # Declare one or more broker connections unusable because connection setup has failed
+    #
+    # === Parameters
+    # options(Hash):: Failure options:
+    #   :brokers(Array):: Identity of brokers
+    #
+    # === Return
+    # res(OperationResult):: Success unless exception is raised
+    def connect_failed(options)
+      options = SerializationHelper.symbolize_keys(options)
+      res = success_result
+      begin
+        if error = @agent.connect_failed(options[:brokers])
+          res = error_result(error)
+        end
+      rescue Exception => e
+        res = error_result("Failed to notify agent that brokers #{options[:brokers]} are unusable: #{e.message}")
+      end
+      res
+    end
+
+  end # AgentManager
+
+end # RightScale
