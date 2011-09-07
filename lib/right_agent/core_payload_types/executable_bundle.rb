95878d043d05c8fd10ae8ee939dfc90ac16513f1
Scott Messier <scott@rightscale.com>
2011-09-06 17:37:18 -0700

    moved resolution of concurrent thread name to ExecutableBundle

--- lib/right_agent/core_payload_types/executable_bundle.rb
+++ lib/right_agent/core_payload_types/executable_bundle.rb
@@ -35,6 +35,9 @@ module RightScale
 
     include Serializable
 
+    # Default thread name when no thread is specified for an executable bundle.
+    DEFAULT_THREAD_NAME = 'default'
+
     # (Array) Collection of RightScripts and chef recipes instantiations
     attr_accessor :executables
 
@@ -53,9 +56,6 @@ module RightScale
 
     # (String) Repose server to use
     attr_accessor :repose_servers
-
-    # (String) Thread name for concurrent execution or nil
-    attr_accessor :thread_name
 
     def initialize(*args)
       @executables           = args[0]
@@ -85,6 +85,19 @@ module RightScale
     def to_s
       desc = @executables.collect { |e| e.nickname }.join(', ') if @executables
       desc ||= 'empty bundle'
+    end
+
+    # Gets the thread name from a bundle, if any. Uses the default thread name for
+    # when a thread name is not specified (for backward compatibility, etc.).
+    #
+    # === Parameters
+    # bundle(ExecutableBundle):: bundle to inspect
+    #
+    # === Return
+    # thread_name(String):: thread name for bundle execution
+    def thread_name(value = nil)
+      @thread_name = value if value
+      return @thread_name || DEFAULT_THREAD_NAME
     end
 
   end
