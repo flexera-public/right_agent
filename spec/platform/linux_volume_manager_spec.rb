f9506355bd1324fa1ff7fd788cfd9a8d225c75a6
Ryan J. Geyer <me@ryangeyer.com>
2012-04-05 16:56:28 -0700

    Added remotely possible period (.) character to device name regex

--- spec/platform/linux_volume_manager_spec.rb
+++ spec/platform/linux_volume_manager_spec.rb
@@ -57,7 +57,18 @@ EOF
           ]
 
           @platform.volume_manager.parse_volumes(blkid_resp).should == volume_hash_ary
+        end
 
+        it 'can parse volumes with periods' do
+          blkid_resp = <<EOF
+/dev/please.dont.do.this: UUID="ee34706d-866f-476e-9da4-6a18745456a4" TYPE="xfs"
+EOF
+
+          volume_hash_ary = [
+            {:device => '/dev/please.dont.do.this', :uuid => 'ee34706d-866f-476e-9da4-6a18745456a4', :type => 'xfs', :filesystem => "xfs"}
+          ]
+
+          @platform.volume_manager.parse_volumes(blkid_resp).should == volume_hash_ary
         end
 
         it 'raises a parser error when blkid output is malformed' do
