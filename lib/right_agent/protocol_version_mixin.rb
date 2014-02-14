ae09d3bb639ae4de4f4840814ecfc60f40af975c
Lee Kirchhoff <lee@rightscale.com>
2014-02-14 11:17:42 -0800

    acu151583 - Simplify ProtocolVersionMixin module

--- lib/right_agent/protocol_version_mixin.rb
+++ lib/right_agent/protocol_version_mixin.rb
@@ -25,49 +25,44 @@ module RightScale
   # Mixin for testing for availability of features in agents based on protocol version
   module ProtocolVersionMixin
 
+    # Mix this module into its own eigenclass to make the module-instance methods
+    # become callable as module methods
+    extend self
+
     # Test whether given version of agent has the protocol version embedded in each
     # packet (this is generally inferred by the version in the received packet not being
     # Packet::DEFAULT_VERSION, which is true of all with version >= 12)
     def can_put_version_in_packet?(version);      version && version != 0 end
-    def self.can_put_version_in_packet?(version); version && version != 0 end
 
     # Test whether given version of agent uses /mapper/query_tags rather than the
     # deprecated TagQuery packet
     def can_use_mapper_query_tags?(version);      version && version >= 8 end
-    def self.can_use_mapper_query_tags?(version); version && version >= 8 end
 
     # Test whether given version of agent can handle a request that is being retried
     # as indicated by a retries count in the Request packet
     def can_handle_request_retries?(version);      version && version >= 9 end
-    def self.can_handle_request_retries?(version); version && version >= 9 end
 
     # Test whether given version of agent can handle serialized identity and queue name
     # that does not incorporate 'nanite'
     def can_handle_non_nanite_ids?(version);      version && version >= 10 end
-    def self.can_handle_non_nanite_ids?(version); version && version >= 10 end
 
     # Test whether given version of agent supports routing results to mapper response queue
     # rather than to the identity queue of the mapper that routed the request
     def can_route_to_response_queue?(version);      version && version >= 10 end
-    def self.can_route_to_response_queue?(version); version && version >= 10 end
 
     # Test whether given version of agent can handle receipt of a result containing an
     # OperationResult with MULTICAST status
     def can_handle_multicast_result?(version);      version && [10, 11].include?(version) end
-    def self.can_handle_multicast_result?(version); version && [10, 11].include?(version) end
 
     # Test whether given version of agent can handle msgpack encoding
     def can_handle_msgpack_result?(version);      version && version >= 12 end
-    def self.can_handle_msgpack_result?(version); version && version >= 12 end
 
     # Test whether given version of agent can handle receipt of a result containing an
     # OperationResult with NON_DELIVERY status
     def can_handle_non_delivery_result?(version);      version && version >= 13 end
-    def self.can_handle_non_delivery_result?(version); version && version >= 13 end
 
     # Test whether given version of agent can handle HTTP communication mode
     def can_handle_http?(version);      version && version >= 23 end
-    def self.can_handle_http?(version); version && version >= 23 end
 
   end # ProtocolVersionMixin
 
